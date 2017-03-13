f8a2ff2e559245fab8cbd93b2f1a8091a1d483a5
Jeffrey Brown <Jeffrey Brown>
2017-03-13 02:21:01 -0400

    small refactor

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,45 +14,49 @@ $("body").on("click", ".ranger-image", function() {
     console.log(this.id);
 });
 
-function spawnRanger () {
-    tempPProfile = {"imageSource": "", "powerBase":0, "attackPower": 0, "healthPoints": 0};
+function spawnRanger(arrayIndex) {
+    tempPProfile = { "imageSource": "", "powerBase": 0, "attackPower": 0, "healthPoints": 0, "index": -1 };
     tempPProfile.imageSource = images.splice(Math.floor(Math.random() * images.length), 1)[0];
     tempPProfile.healthPoints = initialHealths.splice(Math.floor(Math.random() * initialHealths.length), 1)[0];
+    tempPProfile.index = arrayIndex;
     return tempPProfile;
 }
 
-function initializeGame() {
-    for (var i = 0; i < playerProfiles.length; i++) {
-        var tempElement = $("<div>");
-        tempElement.addClass("ranger-image-object");
-        tempElement.attr("id", "ranger" + i);
-        $("#player-on-deck").append(tempElement);
-        tempElement = $("<div>");
-        tempElement.addClass("ranger-image-frame")
-        tempElement.attr("id", "ranger" + i + "-frame");
-        $("#ranger" + i).append(tempElement);
-        tempElement = $("<img>");
-        tempElement.addClass("ranger-image");
-        tempElement.attr("src", "assets/images/" + playerProfiles[i].imageSource);
-        tempElement.attr("data-healthpoints", playerProfiles[i].healthPoints);
-        $("#ranger" + i + "-frame").append(tempElement)
-        tempElement = $("<div>");
-        tempElement.addClass("ranger-image-points");
-        tempElement.attr("id", "ranger" + i + "-points-text");
-        tempElement.text(playerProfiles[i].healthPoints);
-        $("#ranger" + i + "-frame").append(tempElement);
-        tempElement = $("<div>");
-        tempElement.addClass("ranger-spacer");
-        tempElement.text(myNBSP);
-        $("#player-on-deck").append(tempElement);
+function createRangerIcon(element, profileObject) {
+    var tempElement = $("<div>");
+    tempElement.addClass("ranger-image-object");
+    tempElement.attr("id", "ranger" + i);
+    $(element).append(tempElement);
+    tempElement = $("<div>");
+    tempElement.addClass("ranger-image-frame")
+    tempElement.attr("id", "ranger" + i + "-frame");
+    $("#ranger" + i).append(tempElement);
+    tempElement = $("<img>");
+    tempElement.addClass("ranger-image");
+    tempElement.attr("src", "assets/images/" + profileObject.imageSource);
+    tempElement.attr("data-healthpoints", profileObject.healthPoints);
+    $("#ranger" + i + "-frame").append(tempElement)
+    tempElement = $("<div>");
+    tempElement.addClass("ranger-image-points");
+    tempElement.attr("id", "ranger" + i + "-points-text");
+    tempElement.text(profileObject.healthPoints);
+    $("#ranger" + i + "-frame").append(tempElement);
+    tempElement = $("<div>");
+    tempElement.addClass("ranger-spacer");
+    tempElement.text(myNBSP);
+    $(element).append(tempElement);
+}
 
+
+function initializeGame() {
+    // spawn four ranger profiles into the player profile array
+    for (i = 0; i < profileCount; i++) {
+        playerProfiles.push(spawnRanger(i));
+        createRangerIcon("#player-on-deck", playerProfiles[i]);
     }
+    $(".ranger-image-object").css("background-color", "green");
     $("#top-text").text("Click a player for yourself ...");
 }
 
 profileCount = images.length;
-// spawn four ranger profiles into the player profile array
-for (i = 0; i < profileCount; i++) {
-    playerProfiles.push(spawnRanger());
-}
 initializeGame();
